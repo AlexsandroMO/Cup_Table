6175664c9ff2fb8d1bc821c278ed5a9919bd0ce2
AlexsandroMO <sandrobass@hotmail.com>
2022-11-23 18:01:20 -0300

    update

--- Calc.py
+++ Calc.py
@@ -49,6 +49,67 @@ def list_games():
   team_all = soup.find_all('span',{'class':'acronym'})
   flags = soup.find_all('img',{'class':'sports-shield'})
 
+  flag_a = []
+  for i in range(0, len(flags)):
+    if i >= 0 and i < 4:
+      flag_a.append([flags[i]['alt'],flags[i]['src']])
+    elif i >= 16 and i < 20:
+      flag_a.append([flags[i]['alt'],flags[i]['src']])
+    elif i >= 32 and i < 36:
+      flag_a.append([flags[i]['alt'],flags[i]['src']])
+    elif i >= 48 and i < 52:
+      flag_a.append([flags[i]['alt'],flags[i]['src']])
+    elif i >= 64 and i < 68:
+      flag_a.append([flags[i]['alt'],flags[i]['src']])
+    elif i >= 80 and i < 84:
+      flag_a.append([flags[i]['alt'],flags[i]['src']])
+    elif i >= 96 and i < 100:
+      flag_a.append([flags[i]['alt'],flags[i]['src']])
+    elif i >= 112 and i < 116:
+      flag_a.append([flags[i]['alt'],flags[i]['src']])
+
+  flag_b = [
+                    ['ALE','Alemanha'],
+                    ['ARG','Argentina'],
+                    ['AUT','Austrália'],
+                    ['BEL','Bélgica'],
+                    ['BRA','Brasil'],
+                    ['CAN','Canada'],
+                    ['CMR','Camarões'],
+                    ['CRI','Costa Rica'],
+                    ['CRO','Croácia'],
+                    ['DEN','Dinamarca'],
+                    ['EQU','Equador'],
+                    ['ESP','Espanha'],
+                    ['EUA','USA'],
+                    ['FRA','França'],
+                    ['GHA','Gana'],
+                    ['ING','Inglaterra'],
+                    ['IRA','Irã'],
+                    ['JAP','Japão'],
+                    ['KOR','Coreia do Sul'],
+                    ['MAR','Marrocos'],
+                    ['MEX','México'],
+                    ['NED','Holanda'],
+                    ['PGL','País de Gales'],
+                    ['POL','Polônia'],
+                    ['POR','Portugal'],
+                    ['QAT','Catar'],
+                    ['SAU','Arábia Saudita'],
+                    ['SER','Sérvia'],
+                    ['SNG','Senegal'],
+                    ['SWT','Suiça'],
+                    ['TUN','Tunísia'],
+                    ['URU','Uruguai']
+                  ]
+
+  flag_country = []
+  for a in range(0, len(flag_a)):
+    for b in range(0, len(flag_b)):
+      if flag_a[a][0] == flag_b[b][1]:
+        flag_country.append([flag_a[a][0],flag_b[b][0],flag_a[a][1]])
+
+  #---------------------
   array = np.array(np.arange(len(team_all)))
   x = np.where(array%2 == 0)
   y = np.where(array%2 == 1)
@@ -69,26 +130,7 @@ def list_games():
   for read in team_y:
     team_b.append(team_all[read].text)
 
-  array = np.array(np.arange(len(flags)))
-  x = np.where(array%2 == 0)
-  y = np.where(array%2 == 1)
-  flags_x, flags_y = [],[]
-  for a in x:
-    for b in a:
-      flags_x.append(b)
-
-  for a in y:
-    for b in a:
-      flags_y.append(b)
-
-  flag_a = []
-  for read in flags_x:
-    flag_a.append(flags[read]['src'])
-
-  flag_b = []
-  for read in flags_y:
-    flag_b.append(flags[read]['src'])
-
+  #------------
   result_list = []
   increment = 1
   cont_team = 0
@@ -96,27 +138,34 @@ def list_games():
     if cont < len(result)/3:
       if result[cont + increment].contents[0].text != '':
         result_list.append([team_a[cont_team],
-                            flag_a[cont_team],
-                            result[cont + increment].contents[0].text,
-                            result[cont + increment].contents[2].text,
-                            flag_b[cont_team],
-                            team_b[cont_team],
-                            result[cont + increment].find('div',{'class':'details'}).text[:-14]])
+                          '',
+                          result[cont + increment].contents[0].text,
+                          result[cont + increment].contents[2].text,
+                          '',
+                          team_b[cont_team],
+                          result[cont + increment].find('div',{'class':'details'}).text[:-14]])
       else:
         result_list.append([team_a[cont_team],
-                            flag_a[cont_team],
-                            result[cont + increment].contents[0].text,
-                            result[cont + increment].contents[2].text,
-                            flag_b[cont_team],
-                            team_b[cont_team],
+                          '',
+                          result[cont + increment].contents[0].text,
+                          result[cont + increment].contents[2].text,
+                          '',
+                          team_b[cont_team],
                           result[cont + increment].find('div',{'class':'details'}).text])
-        
+      
       increment += 2
       cont_team += 1
 
   for a in result_list:
     a[4] = a[4].replace('h00','h00 ')
 
+  for a in range(0, len(result_list)):
+    for b in range(0, len(flag_country)):
+      if result_list[a][0] == flag_country[b][1]:
+        result_list[a][1] = flag_country[b][2]
+      if result_list[a][5] == flag_country[b][1]:
+        result_list[a][4] = flag_country[b][2]
+        
   result_list
 
   return result_list
--- tableCup/views.py
+++ tableCup/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import pandas as pd
 import Calc as CL
-
+import PandasConvertList as PCL
 
 def Index(request):
 
@@ -12,10 +13,8 @@ def TableTeam(request):
 
     read_table = CL.read_table_status()
 
-    # tabela = []
-    # for a in range(0, len(read_table['POS'])):
-    #     tabela.append([read_table['POS'].loc[a],read_table['Time'].loc[a],read_table['PTS'].loc[a],read_table['J'].loc[a],read_table['V'].loc[a],read_table['E'].loc[a], read_table['D'].loc[a],read_table['GP'].loc[a],read_table['GC'].loc[a],read_table['SG'].loc[a],read_table['percento'].loc[a],read_table['Recentes'].loc[a]])
-
+    # df = pd.read_csv('media/TABLE_GAME.csv')
+    # tabela = PCL.change_in_list(df)
     # print(tabela)
 
     return render(request,'tableCup/table-teams.html', {'read_table':read_table})
